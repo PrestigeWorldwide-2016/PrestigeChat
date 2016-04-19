62c06a20336e41870b7da6a4ed95b7699c59205e
AxialLP7 <axial.jc@gmail.com>
2016-04-19 11:09:26 -0700

    fixed merge conflicts; removed master retaining jc-dev

--- ChatSystem/public/script.js
+++ ChatSystem/public/script.js
@@ -1,64 +1,4 @@
 $(document).ready(function() {
-<<<<<<< HEAD
-
-var   submitPhase1 = 1100,
-      submitPhase2 = 400,
-      logoutPhase1 = 800,
-      $login = $(".login-card"),
-      $app = $(".app");
-      $app.hide();
-
-    $(document).on("click", ".login__submit", function(e) {
-
-    var socket = socketCluster.connect();
-    var username = $('#Username').val();
-    var userpass = $('#Password').val();
-    var user = {
-      uName: username,
-      password: userpass
-    };
-
-    socket.emit('login', user, function (err) {
-
-      if (err) {
-        console.log(err);
-        //location.reload();
-      }
-      else {
-    setTimeout(function() {
-      setTimeout(function() {
-        $app.show();
-//        $app.css("top");
-  //      $app.addClass("active");
-      }, submitPhase2 - 70);
-      setTimeout(function() {
-        $login.hide();
-  //      $login.addClass("inactive");
-        }, submitPhase2);
-
-      }, submitPhase1);
-      ConnectUser();
-    }
-  });
-
-
-function ConnectUser() {
-
-
-        socket.on('error', function (err) {
-          throw 'Socket error - ' + err;
-          });
-
-        socket.on('connect', function () {
-          console.log(username + ' Connected to server');
-          });
-
-
-          var channelName = "BigGroup";
-          var chatChannel = socket.subscribe(channelName);
-          chatChannel.on('subscribeFail', function(err) {
-          console.log('Failed to subscribe to ' + channelName + ' channel due to error: ' + err);
-=======
     var submitPhase1 = 1100,
         submitPhase2 = 400,
         logoutPhase1 = 800,
@@ -105,7 +45,7 @@ function ConnectUser() {
         function ConnectUser() {
             socket.on('error', function (err) {
                 throw 'Socket error - ' + err;
->>>>>>> refs/remotes/origin/jc-dev
+
             });
 
             socket.on('connect', function () {
