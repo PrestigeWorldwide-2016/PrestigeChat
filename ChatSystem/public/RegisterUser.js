d90dd91beca8a7065b1ab6020ce99e33718d1418
Jlira360 <jlira360@gmail.com>
2016-04-26 19:40:58 -0700

    Check for Valid User Credentials

--- ChatSystem/public/RegisterUser.js
+++ ChatSystem/public/RegisterUser.js
@@ -6,10 +6,30 @@ $(document).ready(function() {
 
     var socket = socketCluster.connect();
     var fisrtName = $('#FirstName').val();
+    if (firstName == null || fisrtName.includes(" ")) {
+      console.log("User did not enter Valid Credetials.");
+      window.prompt("Please enter Valid Credetials.");
+    }
     var lastName = $('#LastName').val();
+      if (lastName == null || lastName.includes(" ")) {
+      console.log("User did not enter Valid Credetials.");
+      window.prompt("Please enter Valid Credetials.");
+    }
     var email = $('#Email').val();
+      if (email == null || email.includes(" ")) {
+      console.log("User did not enter Valid Credetials.");
+      window.prompt("Please enter Valid Credetials.");
+    }
     var username = $('#Username').val();
+      if (username == null || username.includes(" ")) {
+      console.log("User did not enter Valid Credetials.");
+      window.prompt("Please enter Valid Credetials.");
+    }
     var userpass = $('#Password').val();
+      if (userpass == null || userpass.includes(" ")) {
+      console.log("User did not enter Valid Credetials.");
+      window.prompt("Please enter Valid Credetials.");
+    }
 
     var user = {
       fName: fisrtName,
@@ -19,6 +39,8 @@ $(document).ready(function() {
       password: userpass
     };
 
+
+    /*
     if ((fisrtName == null) || (lastName == null) 
          || (email == null) || (username == null) 
          || (userpass == null)) {
@@ -26,6 +48,7 @@ $(document).ready(function() {
       window.prompt("Please enter Valid Credetials.");
       location.reload();
     }
+    */
 
     else {
 
