a6ff971cc051e09ca6bf1cae551a0aae478fa998
AxialLP7 <axial.jc@gmail.com>
2016-04-30 22:58:50 -0700

    spacing issues RIP

--- ChatSystem/public/RegisterUser.js
+++ ChatSystem/public/RegisterUser.js
@@ -11,30 +11,29 @@ $(document).ready(function() {
         window.prompt("Please enter Valid info")
         location.reload();
      } else {
-        var socket = socketCluster.connect();
-        var firstName = $('#FirstName').val();
-        var lastName = $('#LastName').val();
-        var email = $('#Email').val();
-        var username = $('#Username').val();
-        var userpass = $('#Password').val();
-
-        var user = {
-          fName: firstName,
-          lName: lastName,
-          email: email,
-          uName: username,
-          password: userpass
-        };
+         var socket = socketCluster.connect();
+         var firstName = $('#FirstName').val();
+         var lastName = $('#LastName').val();
+         var email = $('#Email').val();
+         var username = $('#Username').val();
+         var userpass = $('#Password').val();
+         
+         var user = {
+             fName: firstName,
+             lName: lastName,
+             email: email,
+             uName: username,
+             password: userpass
+         };
+         socket.emit('register', user, function (err) {
+             if (err) {
+                 console.log(err);
+                 window.alert(err);
+                 location.reload();
+             } else {
+                 window.prompt("Successfully Registered " + username + ".");
+                 location.href = "/";
+             }
+         });
      }
-        socket.emit('register', user, function (err) {
-            if (err) {
-                console.log(err);
-                window.alert(err);
-                location.reload();
-            } else {
-                window.prompt("Successfully Registered " + username + ".");
-                location.href = "/";
-            }
-        });
-    });
 });
