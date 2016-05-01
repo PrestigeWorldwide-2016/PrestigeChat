04efc7752f1c0fb5839613f44bf5b8f560e27530
AxialLP7 <axial.jc@gmail.com>
2016-04-30 22:53:53 -0700

    stupid spacing issue

--- ChatSystem/public/script.js
+++ ChatSystem/public/script.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
       $app = $(".app");
 
     $app.hide();
-
     
 	$(document).on("click", ".login__submit", function(e) {
         var that = this; // what's this?
