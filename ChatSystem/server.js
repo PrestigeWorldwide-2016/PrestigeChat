18ae03096f2ce6d844b5473bd8e90f465e816714
meyer067 <meyer067@cougars.csusm.edu>
2016-05-08 19:21:40 -0700

    adding code to server for openshift

--- ChatSystem/server.js
+++ ChatSystem/server.js
@@ -11,3 +11,10 @@ var socketCluster = new SocketCluster({
   socketChannelLimit: 1000,
   crashWorkerOnError: argv['auto-reboot'] != false
 });
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
